eb0c97574355d5eefb3034349863effbc6f26c05
Altaf Multani <altafmultani16@gmail.com>
2022-09-20 13:14:55 +0530

    firt Commit
    firt Commit

---
+++ CameraSwiftUi/CameraSwiftUi/CameraSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraSwiftUiApp.swift
+//  CameraSwiftUi
+//
+//  Created by Javid Multani on 14/07/22.
+//
+
+import SwiftUI
+
+ @main
+struct CameraSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
